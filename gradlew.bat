a96bde7aea284d2916afa527cf1ee65d9157962b
xhamzar <nabhel.bege.com>
2025-06-20 13:21:20 +0700

    Add Gradle wrapper

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@IF "%DEBUG%" == "" ECHO OFF
+@REM ##########################################################################
+@REM
+@REM  Gradle start up script for Windows
+@REM
+@REM ##########################################################################
+@REM ... (isi lengkap gradlew.bat, bisa generate otomatis via Android Studio atau copy dari project Android lain)
